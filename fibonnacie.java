48794e215fae5fb53bb5b5edeb175915da71bfe2
SAI GANESHAN MANIKANDAN <sm2461@srmist.edu.in>
2025-03-04 04:09:57 +0530

    Added Fibonacci series program and project files

---
+++ fibonnacie.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class fibonnacie{
+    public static void main(String args[])
+    {
+        int a =0;int b =1;
+        int n = 7;
+        int count = 2;
+        while(count<=n)
+        {
+            int temp = b;
+            b =a+b;
+            a = temp;count++;
+        }
+        System.out.println("the value is "+b);
+    }
+}
